7dce9e05d99c1061c48a1b7d6ae583b987ad4f27
Mike Zamayias <mzamagias@gmail.com>
2020-12-08 17:24:59 +0200

    updated script to isntall dart and deleted two shell scripts

--- python-scripts/install_dart.py
+++ python-scripts/install_dart.py
@@ -19,7 +19,7 @@ commands = (
     "sudo apt update",
     "sudo apt install -y apt-transport-https",
     "sudo sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'",
-    "sudo sh - c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'",
+    "sudo sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'",
     "sudo apt update",
     "sudo apt install -y dart",
     "echo 'export PATH=\"$PATH:/usr/lib/dart/bin\"' >> ~/.zshrc"
