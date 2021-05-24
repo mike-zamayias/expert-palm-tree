3239018d0a7ad7148f959ca76488d615a2795dd8
Mike Zamayias <mzamagias@gmail.com>
2021-05-25 00:42:15 +0300

    updated alarm file

---
+++ python-scripts/alarmy/alarmy.py
@@ -0,0 +1,17 @@
+from time import sleep
+from datetime import datetime
+from os import system as run
+
+alarm = 'Navy Seal Commander explains why wake up at 4am.mp4'
+alarm_time = [6, 0]
+print(f'Alarm time:\t{alarm_time[0]:02d}:{alarm_time[1]:02d}')
+
+while True:
+    current_time = [datetime.now.hour, datetime.now.minute]
+    print(f'\rCurrent time:\t{current_time[0]:02d}:{current_time[1]:02d}', end='\r')
+    if current_time == alarm_time:
+        break
+    else:
+        sleep(1)
+
+run(f'vlc {alarm}')
