0b2ff98fa10913d7f39a6c3b5bfa7eb5592b4e41
Mike Zamayias <mzamagias@gmail.com>
2021-05-25 01:44:10 +0300

    changed the time when the alarm goes off and added a line

--- python-scripts/alarmy/alarmy.py
+++ python-scripts/alarmy/alarmy.py
@@ -3,8 +3,10 @@ from time import sleep
 from datetime import datetime
 from os import system as run
 
+run('clear')
+
 alarm = 'Navy Seal Commander explains why wake up at 4am.mp4'
-alarm_time = [5, 30]
+alarm_time = [5, 45]
 print(f'Alarm time:\t{alarm_time[0]:02d}:{alarm_time[1]:02d}')
 
 while True:
