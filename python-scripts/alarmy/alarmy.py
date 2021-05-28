2e0b8225b692e5b2604200dea66ef58248cb2a61
Mike Zamayias <mzamagias@gmail.com>
2021-05-28 06:37:29 +0300

    updated the script. it now works a bit better. needs to be documented.

--- python-scripts/alarmy/alarmy.py
+++ python-scripts/alarmy/alarmy.py
@@ -1,20 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+from datetime import datetime as dt
 from time import sleep
-from datetime import datetime
-from os import system as run
+from random import choice
+from os import system, listdir
 
-run('clear')
 
-alarm = 'Navy Seal Commander explains why wake up at 4am.mp4'
-alarm_time = [5, 45]
-print(f'Alarm time:\t{alarm_time[0]:02d}:{alarm_time[1]:02d}')
+def now_time():
+    return f'{dt.now().hour:02d}:{dt.now().minute:02d}'
 
-while True:
-    current_time = [datetime.now().hour, datetime.now().minute]
-    print(f'\rCurrent time:\t{current_time[0]:02d}:{current_time[1]:02d}', end='\r')
-    if current_time == alarm_time:
-        break
-    else:
-        sleep(1)
 
-run(f'vlc {alarm}')
+def alarm():
+    alarm_time = str(input('Enter when the alarm should go off:\t'))
+    print(f'Alarm set for:\t{alarm_time}')
+    while True:
+        if now_time() != alarm_time:
+            print(f'\rCurrent time:\t{now_time()}', end='\r')
+            sleep(1)
+        else:
+            #   os.system(f'vlc "How to get people to DO WHAT YOU WANT _ Ryan Serhant Vlog #54.mp4"')
+            print(f'vlc "{media()}"')
+            system(f'vlc "{media()}"')
+
+
+def media():
+    media_files = [
+        item for item in listdir()
+        if 'mp4' in item or 'mkv' in item
+    ]
+    return choice(media_files)
+
+
+alarm()
